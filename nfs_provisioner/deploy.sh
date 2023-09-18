5667998e8e66c042b4e6988d12c84488fde79009
sean <spi3@pm.me>
2023-09-18 01:12:38 -0400

    change server ip

--- nfs_provisioner/deploy.sh
+++ nfs_provisioner/deploy.sh
@@ -1,6 +1,6 @@
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-    --set nfs.server=10.10.10.2 \
+    --set nfs.server=10.0.0.2 \
     --set nfs.path=/mnt/user/kubernetes/volumes \
     --set storageClass.name=nfs-sc \
     --set storageclass.kubernetes.io/is-default-class="true"
