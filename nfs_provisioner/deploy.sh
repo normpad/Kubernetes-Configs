52e7f58778f1bd1e8c1594ea40dd04360c1f33d5
Sean <normpad48@gmail.com>
2024-05-15 22:06:51 -0400

    Removed unused configs

--- nfs_provisioner/deploy.sh
+++ nfs_provisioner/deploy.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
     --set nfs.server=10.0.0.2 \
     --set nfs.path=/mnt/user/kubernetes/volumes \
     --set storageClass.name=nfs-sc \
-    --set storageclass.kubernetes.io/is-default-class="true"
+    --set storageclass.kubernetes.io/is-default-class="true" \
+    --set storageClass.pathPattern="$\{.PVC.name\}" \
+    --set storageClass.onDelete=retain \
 
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 kubectl patch storageclass nfs-sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
