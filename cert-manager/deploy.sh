bcc2e915c4f034b5cfc8f254bdfec584bacb171b
Sean <normpad48@gmail.com>
2024-05-19 03:01:12 -0400

    Removed cert manager

--- cert-manager/deploy.sh
+++
@@ -1,3 +0,0 @@
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.4/cert-manager.crds.yaml
-helm repo add jetstack https://charts.jetstack.io
-helm install --create-namespace cert-manager --namespace cert-manager --version v1.12.4 jetstack/cert-manager
