02c65f0db867f2f63a37db3cc965f7f65f0e2a12
Sean <normpad48@gmail.com>
2024-05-22 01:35:09 -0400

    Fix prometheus deployment

--- prometheus/deploy.sh
+++ prometheus/deploy.sh
@@ -1,3 +1,4 @@
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install --create-namespace -n prometheus prometheus prometheus-community/kube-prometheus-stack
+helm upgrade --install --create-namespace -n prometheus prometheus prometheus-community/kube-prometheus-stack
